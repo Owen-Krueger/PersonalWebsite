aeab5306577d663bd13a9a46aefe06419bff7d4e
Owen Krueger <github@owenkrueger.dev>
2023-11-20 19:38:04 -0600

    Properly cd out

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{razor,cshtml,html}"],
+  content: ["./**/*.{razor,cshtml,html}"],
   theme: {
     extend: {},
   },
